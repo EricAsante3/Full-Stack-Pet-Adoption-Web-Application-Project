73a2381727e2010be5bacf7d2809bbb7b0744761
Eric Asante <Eric.Asante@uconn.edu>
2024-11-04 14:11:37 -0500

    pylint complete

--- Backend/PetFuc.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, json, request, jsonify
-
-
-def Get_Random_Pet():
-    randomint = "9" # APi generates random interger that represents a PET ID
-    # Api accesses database and return information in data base
-    #Retrun information such as profile picture on the pet accosiated with the random ID 
-    return jsonify({"id": randomint,"name":"sam","sex":"male","Age":"8", "location":"Connecticwut", "Breed": "Pug"}) # Return dog information
-
